e38cab5a5a170395dac620c9676a0f8cc9c56efe
Ryan Benasutti <firey45@gmail.com>
2018-11-02 19:31:15 -0400

    Copy over CollectionsUtil from KinematicsChef

--- Core/src/main/kotlin/com/neuronrobotics/bowlerkernel/util/CollectionsUtil.kt
+++ Core/src/main/kotlin/com/neuronrobotics/bowlerkernel/util/CollectionsUtil.kt
@@ -1,15 +1,17 @@
 /*
- * Copyright 2017 Ryan Benasutti
+ * Copyright 2018 Ryan Benasutti
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-@file:Suppress("TooManyFunctions")
+@file:Suppress("TooManyFunctions", "UndocumentedPublicFunction")
+
 package com.neuronrobotics.bowlerkernel.util
 
 import com.google.common.collect.ImmutableList
 import com.google.common.collect.ImmutableListMultimap
+import com.google.common.collect.ImmutableMap
 import com.google.common.collect.ImmutableSet
 import com.google.common.collect.ImmutableSetMultimap
 import com.google.common.collect.Multimap
@@ -58,8 +60,30 @@ fun <K, V> immutableSetMultimapOf(vararg pairs: Pair<K, V>): ImmutableSetMultima
 
 fun <K, V> emptyImmutableSetMultimap(): ImmutableSetMultimap<K, V> = ImmutableSetMultimap.of()
 
+fun <K, V> Iterable<Pair<K, V>>.toImmutableMap(): ImmutableMap<K, V> =
+    ImmutableMap.copyOf(toMap())
+
+fun <K, V> immutableMapOf(vararg elements: Pair<K, V>): ImmutableMap<K, V> =
+    ImmutableMap.copyOf(elements.toMap())
+
+fun <K, V> emptyImmutableMap(): ImmutableMap<K, V> = ImmutableMap.of()
+
 operator fun <T> ImmutableList<T>.plus(other: ImmutableList<T>): ImmutableList<T> =
     ImmutableList.builder<T>()
         .addAll(this)
         .addAll(other)
+        .build()
+
+operator fun <K, V> ImmutableListMultimap<K, V>.plus(other: ImmutableListMultimap<K, V>):
+    ImmutableListMultimap<K, V> =
+    ImmutableListMultimap.builder<K, V>()
+        .putAll(this)
+        .putAll(other)
+        .build()
+
+operator fun <K, V> ImmutableSetMultimap<K, V>.plus(other: ImmutableSetMultimap<K, V>):
+    ImmutableSetMultimap<K, V> =
+    ImmutableSetMultimap.builder<K, V>()
+        .putAll(this)
+        .putAll(other)
         .build()
