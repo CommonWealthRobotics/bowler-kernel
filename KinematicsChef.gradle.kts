f91dc286861f6d477b10e7a8dfc01db36db22d30
Ryan Benasutti <firey45@gmail.com>
2018-12-01 18:23:57 -0500

    v0.0.8

--- KinematicsChef.gradle.kts
+++ KinematicsChef.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 
 object Versions {
     const val ktlintVersion = "0.29.0"
-    const val kinematicsChefVersion = "0.0.7"
+    const val kinematicsChefVersion = "0.0.8"
 }
 
 allprojects {
