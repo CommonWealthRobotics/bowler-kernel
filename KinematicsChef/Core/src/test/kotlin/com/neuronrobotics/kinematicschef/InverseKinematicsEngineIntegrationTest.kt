0bf08ba98892decee9a9c2c822ccec77104907cd
Ryan Benasutti <firey45@gmail.com>
2018-12-06 15:27:20 -0500

    Enable InverseKinematicsEngineIntegrationTest

--- KinematicsChef/Core/src/test/kotlin/com/neuronrobotics/kinematicschef/InverseKinematicsEngineIntegrationTest.kt
+++ KinematicsChef/Core/src/test/kotlin/com/neuronrobotics/kinematicschef/InverseKinematicsEngineIntegrationTest.kt
@@ -10,14 +10,12 @@ package com.neuronrobotics.kinematicschef
 import com.neuronrobotics.sdk.addons.kinematics.DHLink
 import com.neuronrobotics.sdk.addons.kinematics.math.TransformNR
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class InverseKinematicsEngineIntegrationTest {
 
     @Test
-    @Disabled
     fun `one invalid spherical wrist`() {
         val chain = TestUtil.makeMockChain().apply {
             addLink(DHLink(10.0, 0.0, 0.0, -90.0))
