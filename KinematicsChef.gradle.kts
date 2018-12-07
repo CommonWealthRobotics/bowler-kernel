e2a74d732c33a0d86aa6c274678717342e0716ea
Ryan Benasutti <firey45@gmail.com>
2018-12-07 15:52:10 -0500

    Publish version 9 with fat jar

--- KinematicsChef.gradle.kts
+++ KinematicsChef.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 
 object Versions {
     const val ktlintVersion = "0.29.0"
-    const val kinematicsChefVersion = "0.0.8"
+    const val kinematicsChefVersion = "0.0.9"
 }
 
 allprojects {
