804ba2dbf3c33789046424e549456903c77bcf02
Ryan Benasutti <firey45@gmail.com>
2018-12-12 10:46:06 -0500

    Version 11

--- KinematicsChef/Core/Core.gradle.kts
+++ KinematicsChef/Core/Core.gradle.kts
@@ -16,25 +16,25 @@ dependencies {
     api(group = "org.ejml", name = "ejml-all", version = "0.36")
 
     api(group = "io.arrow-kt", name = "arrow-core", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-syntax", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-typeclasses", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-data", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-instances-core", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-instances-data", version = arrow_version)
-    kapt(group = "io.arrow-kt", name = "arrow-annotations-processor", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-free", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-mtl", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-effects", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-effects-rx2", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-effects-reactor", version = arrow_version)
-    implementation(
-        group = "io.arrow-kt",
-        name = "arrow-effects-kotlinx-coroutines",
-        version = arrow_version
-    )
-    implementation(group = "io.arrow-kt", name = "arrow-optics", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-generic", version = arrow_version)
-    implementation(group = "io.arrow-kt", name = "arrow-recursion", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-syntax", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-typeclasses", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-data", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-instances-core", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-instances-data", version = arrow_version)
+//    kapt(group = "io.arrow-kt", name = "arrow-annotations-processor", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-free", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-mtl", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-effects", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-effects-rx2", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-effects-reactor", version = arrow_version)
+//    implementation(
+//        group = "io.arrow-kt",
+//        name = "arrow-effects-kotlinx-coroutines",
+//        version = arrow_version
+//    )
+//    implementation(group = "io.arrow-kt", name = "arrow-optics", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-generic", version = arrow_version)
+//    implementation(group = "io.arrow-kt", name = "arrow-recursion", version = arrow_version)
 
     implementation(group = "com.google.guava", name = "guava", version = "25.0-jre")
     implementation(group = "com.google.inject", name = "guice", version = "4.1.0")
