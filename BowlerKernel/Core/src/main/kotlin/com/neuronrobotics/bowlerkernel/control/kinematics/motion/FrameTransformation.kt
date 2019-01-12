7782256f04fb3d7015528c4b905a6b0d7f19a51c
Ryan Benasutti <firey45@gmail.com>
2019-01-12 16:45:06 -0500

    Add more creation methods to frametransformation

--- BowlerKernel/Core/src/main/kotlin/com/neuronrobotics/bowlerkernel/control/kinematics/motion/FrameTransformation.kt
+++ BowlerKernel/Core/src/main/kotlin/com/neuronrobotics/bowlerkernel/control/kinematics/motion/FrameTransformation.kt
@@ -19,8 +19,18 @@ private constructor(private val mat: SimpleMatrix) {
 
     private val data = DoubleArray(mat.numRows() * mat.numCols()) { mat[it] }
 
+    /**
+     * Extracts the translation component.
+     *
+     * @return A 3x1 translation matrix.
+     */
     fun getTranslation() = mat.extractMatrix(0, 3, 3, 4)
 
+    /**
+     * Extracts the rotation component.
+     *
+     * @return A 3x3 rotation matrix.
+     */
     fun getRotation() = mat.extractMatrix(0, 3, 0, 3)
 
     operator fun plus(other: FrameTransformation) = FrameTransformation(mat + other.mat)
@@ -42,8 +52,21 @@ private constructor(private val mat: SimpleMatrix) {
 
     companion object {
 
+        /**
+         * Constructs the identity frame transformation.
+         *
+         * @return The identity frame transformation.
+         */
         fun identity() = FrameTransformation(SimpleMatrix.identity(4))
 
+        /**
+         * Constructs a frame transformation from a translation.
+         *
+         * @param x The x-axis translation.
+         * @param y The y-axis translation.
+         * @param z The z-axis translation.
+         * @return A translation frame transformation.
+         */
         fun fromTranslation(x: Number, y: Number, z: Number) =
             FrameTransformation(SimpleMatrix.identity(4).apply {
                 this[0, 3] = x.toDouble()
@@ -51,16 +74,56 @@ private constructor(private val mat: SimpleMatrix) {
                 this[2, 3] = z.toDouble()
             })
 
+        /**
+         * Constructs a frame transformation from a translation.
+         *
+         * @param translation A 3x1 column vector representing the x, y, and z axis translations.
+         * @return A translation frame transformation.
+         */
+        fun fromTranslation(translation: SimpleMatrix): FrameTransformation {
+            require(translation.numRows() == 3)
+            require(translation.numCols() == 1)
+
+            return FrameTransformation(SimpleMatrix.identity(4).apply {
+                this[0, 3] = translation[0, 0]
+                this[1, 3] = translation[1, 0]
+                this[2, 3] = translation[2, 0]
+            })
+        }
+
+        /**
+         * Constructs a frame transformation from an Euler rotation. Uses ZYX rotation order.
+         *
+         * @param x The rotation around the x-axis.
+         * @param y The rotation around the y-axis.
+         * @param z The rotation around the z-axis.
+         * @return A rotation frame transformation.
+         */
         fun fromRotation(x: Number, y: Number, z: Number) =
-            FrameTransformation(SimpleMatrix.identity(4).apply {
-                val rotMat = getRotationMatrix(x, y, z)
-                for (row in 0 until rotMat.numRows()) {
-                    for (col in 0 until rotMat.numCols()) {
-                        this[row, col] = rotMat[row, col]
+            fromRotation(getRotationMatrix(x, y, z))
+
+        /**
+         * Constructs a frame transformation from a rotation.
+         *
+         * @param rotation A 3x3 rotation matrix.
+         * @return A rotation frame transformation.
+         */
+        fun fromRotation(rotation: SimpleMatrix): FrameTransformation {
+            require(rotation.numRows() == 3)
+            require(rotation.numCols() == 3)
+
+            return FrameTransformation(SimpleMatrix.identity(4).apply {
+                for (row in 0 until 3) {
+                    for (col in 0 until 3) {
+                        this[row, col] = rotation[row, col]
                     }
                 }
             })
+        }
 
+        /**
+         * A Klaxon JSON converter.
+         */
         val converter = object : Converter {
             override fun canConvert(cls: Class<*>) = cls == FrameTransformation::class.java
 
