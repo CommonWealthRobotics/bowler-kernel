fc85d562cc1f59e1574fc6696a64e42bf5dbbeda
Ryan Benasutti <firey45@gmail.com>
2019-01-28 21:15:01 -0500

    Fix license in bintray publishing

--- kinematics-chef.gradle.kts
+++ kinematics-chef.gradle.kts
@@ -3,12 +3,10 @@ import Kinematics_chef_gradle.Versions.kinematicsChefVersion
 import Kinematics_chef_gradle.Versions.ktlintVersion
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.github.spotbugs.SpotBugsTask
-import com.jfrog.bintray.gradle.tasks.BintrayUploadTask
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.gradle.api.tasks.testing.Test
 import org.gradle.util.GFileUtils
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     jacoco
@@ -36,7 +34,7 @@ allprojects {
 val coreProject = project(":core")
 
 val kotlinProjects = setOf(
-    coreProject
+        coreProject
 )
 
 val javaProjects = setOf<Project>() + kotlinProjects
@@ -117,10 +115,10 @@ configure(javaProjects) {
 
     dependencies {
         fun junitJupiter(name: String, version: String = "5.2.0") =
-            create(group = "org.junit.jupiter", name = name, version = version)
+                create(group = "org.junit.jupiter", name = name, version = version)
 
         fun testFx(name: String, version: String = "4.0.+") =
-            create(group = "org.testfx", name = name, version = version)
+                create(group = "org.testfx", name = name, version = version)
 
         "testCompile"(junitJupiter(name = "junit-jupiter-api"))
         "testCompile"(junitJupiter(name = "junit-jupiter-engine"))
@@ -131,9 +129,9 @@ configure(javaProjects) {
         "testCompile"(group = "org.mockito", name = "mockito-core", version = "2.12.0")
 
         "testRuntime"(
-            group = "org.junit.platform",
-            name = "junit-platform-launcher",
-            version = "1.0.0"
+                group = "org.junit.platform",
+                name = "junit-platform-launcher",
+                version = "1.0.0"
         )
         "testRuntime"(testFx(name = "openjfx-monocle", version = "8u76-b04"))
     }
@@ -166,20 +164,20 @@ configure(javaProjects) {
 
         if (project.hasProperty("jenkinsBuild") || project.hasProperty("headless")) {
             jvmArgs = listOf(
-                "-Djava.awt.headless=true",
-                "-Dtestfx.robot=glass",
-                "-Dtestfx.headless=true",
-                "-Dprism.order=sw",
-                "-Dprism.text=t2k"
+                    "-Djava.awt.headless=true",
+                    "-Dtestfx.robot=glass",
+                    "-Dtestfx.headless=true",
+                    "-Dprism.order=sw",
+                    "-Dprism.text=t2k"
             )
         }
 
         testLogging {
             events(
-                TestLogEvent.FAILED,
-                TestLogEvent.PASSED,
-                TestLogEvent.SKIPPED,
-                TestLogEvent.STARTED
+                    TestLogEvent.FAILED,
+                    TestLogEvent.PASSED,
+                    TestLogEvent.SKIPPED,
+                    TestLogEvent.STARTED
             )
             displayGranularity = 0
             showExceptions = true
@@ -210,8 +208,8 @@ configure(javaProjects) {
             endWithNewline()
             @Suppress("INACCESSIBLE_TYPE")
             licenseHeaderFile(
-                "${rootProject.rootDir}/config/spotless/kinematicschef.license",
-                spotlessLicenseHeaderDelimiter
+                    "${rootProject.rootDir}/config/spotless/kinematicschef.license",
+                    spotlessLicenseHeaderDelimiter
             )
         }
     }
@@ -255,9 +253,9 @@ configure(kotlinProjects) {
         "compile"(kotlin("stdlib", kotlinVersion))
         "compile"(kotlin("reflect", kotlinVersion))
         "compile"(
-            group = "org.jetbrains.kotlinx",
-            name = "kotlinx-coroutines-core",
-            version = "1.0.0"
+                group = "org.jetbrains.kotlinx",
+                name = "kotlinx-coroutines-core",
+                version = "1.0.0"
         )
 
         "testCompile"(kotlin("test", kotlinVersion))
@@ -268,7 +266,7 @@ configure(kotlinProjects) {
         kotlinOptions {
             jvmTarget = "1.8"
             freeCompilerArgs =
-                listOf("-Xjvm-default=enable", "-progressive", "-XXLanguage:+InlineClasses")
+                    listOf("-Xjvm-default=enable", "-progressive", "-XXLanguage:+InlineClasses")
         }
     }
 
@@ -283,15 +281,15 @@ configure(kotlinProjects) {
             configurations {
                 "apiElements" {
                     outgoing
-                        .variants
-                        .getByName("classes")
-                        .artifact(
-                            mapOf(
-                                "file" to compileKotlin.destinationDir,
-                                "type" to "java-classes-directory",
-                                "builtBy" to compileKotlin
+                            .variants
+                            .getByName("classes")
+                            .artifact(
+                                    mapOf(
+                                            "file" to compileKotlin.destinationDir,
+                                            "type" to "java-classes-directory",
+                                            "builtBy" to compileKotlin
+                                    )
                             )
-                        )
                 }
             }
         }
@@ -305,8 +303,8 @@ configure(kotlinProjects) {
             endWithNewline()
             @Suppress("INACCESSIBLE_TYPE")
             licenseHeaderFile(
-                "${rootProject.rootDir}/config/spotless/kinematicschef.license",
-                spotlessLicenseHeaderDelimiter
+                    "${rootProject.rootDir}/config/spotless/kinematicschef.license",
+                    spotlessLicenseHeaderDelimiter
             )
         }
     }
@@ -314,8 +312,8 @@ configure(kotlinProjects) {
     detekt {
         toolVersion = "1.0.0-RC12"
         input = files(
-            "src/main/kotlin",
-            "src/test/kotlin"
+                "src/main/kotlin",
+                "src/test/kotlin"
         )
         parallel = true
         config = files("${rootProject.rootDir}/config/detekt/config.yml")
@@ -375,7 +373,7 @@ configure(publishedProjects) {
             name = "kinematics-chef"
             userOrg = "commonwealthrobotics"
             publish = true
-            setLicenses("MPL-2.0")
+            setLicenses("LGPL-3.0")
             vcsUrl = "https://github.com/CommonWealthRobotics/kinematics-chef.git"
             githubRepo = "https://github.com/CommonWealthRobotics/kinematics-chef"
             with(version) {
@@ -428,13 +426,13 @@ tasks.wrapper {
  * Configures the [publishing][org.gradle.api.publish.PublishingExtension] project extension.
  */
 fun Project.`publishing`(configure: org.gradle.api.publish.PublishingExtension.() -> Unit) =
-    extensions.configure("publishing", configure)
+        extensions.configure("publishing", configure)
 
 /**
  * Configures the [checkstyle][org.gradle.api.plugins.quality.CheckstyleExtension] project extension.
  */
 fun Project.`checkstyle`(configure: org.gradle.api.plugins.quality.CheckstyleExtension.() -> Unit) =
-    extensions.configure("checkstyle", configure)
+        extensions.configure("checkstyle", configure)
 
 /**
  * Retrieves the [java][org.gradle.api.plugins.JavaPluginConvention] project convention.
@@ -446,10 +444,10 @@ val Project.`java`: org.gradle.api.plugins.JavaPluginConvention
  * Configures the [kotlin][org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension] project extension.
  */
 fun Project.`kotlin`(configure: org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension.() -> Unit): Unit =
-    extensions.configure("kotlin", configure)
+        extensions.configure("kotlin", configure)
 
 /**
  * Configures the [detekt][io.gitlab.arturbosch.detekt.extensions.DetektExtension] extension.
  */
 fun org.gradle.api.Project.`detekt`(configure: io.gitlab.arturbosch.detekt.extensions.DetektExtension.() -> Unit): Unit =
-    (this as org.gradle.api.plugins.ExtensionAware).extensions.configure("detekt", configure)
+        (this as org.gradle.api.plugins.ExtensionAware).extensions.configure("detekt", configure)
