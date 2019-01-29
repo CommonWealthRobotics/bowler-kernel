b8ac411d6ae290a203d5cfdc3289a3a7466a5ce5
Ryan Benasutti <firey45@gmail.com>
2019-01-28 21:04:02 -0500

    Move to LGPL

--- README.md
+++ README.md
@@ -4,4 +4,4 @@
 [![Build Status](https://travis-ci.org/CommonWealthRobotics/kinematics-chef.svg?branch=master)](https://travis-ci.org/CommonWealthRobotics/kinematics-chef)
 [![Build status](https://ci.appveyor.com/api/projects/status/69ryb38xhj0ckbru?svg=true)](https://ci.appveyor.com/project/Octogonapus/kinematics-chef)
 [![codecov](https://codecov.io/gh/CommonWealthRobotics/kinematics-chef/branch/master/graph/badge.svg)](https://codecov.io/gh/CommonWealthRobotics/kinematics-chef)
-[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)
+[![License: LGPL-3.0](https://img.shields.io/github/license/CommonWealthRobotics/kinematics-chef.svg)](https://img.shields.io/github/license/CommonWealthRobotics/kinematics-chef.svg)
--- core/src/test/kotlin/com/neuronrobotics/kinematicschef/InverseKinematicsEngineTest.kt
+++ core/src/test/kotlin/com/neuronrobotics/kinematicschef/InverseKinematicsEngineTest.kt
@@ -1,7 +1,18 @@
 /*
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * This file is part of kinematics-chef.
+ *
+ * kinematics-chef is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * kinematics-chef is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with kinematics-chef.  If not, see <https://www.gnu.org/licenses/>.
  */
 package com.neuronrobotics.kinematicschef
 
