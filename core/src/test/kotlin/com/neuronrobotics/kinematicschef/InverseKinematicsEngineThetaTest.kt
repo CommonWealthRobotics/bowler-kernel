9ad121993ea7e2c8f553110a1020acf64a8459aa
Jason McKinney <jmckinney@cra.com>
2019-02-26 11:52:27 -0500

    Move and rename test cases

--- core/src/test/kotlin/com/neuronrobotics/kinematicschef/InverseKinematicsEngineThetaTest.kt
+++ core/src/test/kotlin/com/neuronrobotics/kinematicschef/InverseKinematicsEngineThetaTest.kt
@@ -7,8 +7,9 @@ import com.neuronrobotics.kinematicschef.util.length
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
 import kotlin.math.absoluteValue
+import kotlin.test.assert
 
-class CmmArmTest {
+class InverseKinematicsEngineThetaTest {
   private val cmmInputArm = ScriptingEngine.gitScriptRun(
           "https://gist.github.com/NotOctogonapus/c3fc39308a506d4cb1cd7297193c41e7",
           "InputArmBase_copy.xml",
@@ -34,12 +35,15 @@ class CmmArmTest {
   }
 
   @Test
-  fun `test compute theta1` () {
+  fun `test compute cmm theta1` () {
     (0..314).forEach {
       val wristCenter = cmmParams.forwardKinematics(arrayOf(it * 0.01, 0.0, 0.0, 0.0, 0.0, 0.0).toDoubleArray())
       assert (((cmmParams.computeTheta1(wristCenter)[0] - it * 0.01)) < 0.001)
     }
+  }
 
+  @Test
+  fun `test compute puma theta1` () {
     (0..314).forEach {
       val wristCenter = pumaParams.forwardKinematics(arrayOf(it * 0.01, 0.0, 0.0, 0.0, 0.0, 0.0).toDoubleArray())
       assert (((pumaParams.computeTheta1(wristCenter)[0] - it * 0.01)) < 0.001)
@@ -47,10 +51,28 @@ class CmmArmTest {
   }
 
   @Test
-  fun `test compute theta 23` () {
+  fun `test compute cmm theta 23` () {
     val wristCenter = cmmParams.subList(0, 4).forwardKinematics(arrayOf(0.0, 0.0, 0.0, 0.0).toDoubleArray())
-    val theta23 = cmmParams.computeTheta23(wristCenter, 0.0)
+    val thetas23 = cmmParams.computeTheta23(wristCenter, 0.0)
 
-    val newCenter = cmmParams.subList(0, 4).forwardKinematics(arrayOf(0.0, theta23[0][0], theta23[0][1], 0.0).toDoubleArray())
+    val newCenter = cmmParams.subList(0, 4).forwardKinematics(arrayOf(0.0, thetas23[1][0], thetas23[1][1], 0.0).toDoubleArray())
+
+    val wristCenterVector = wristCenter.cols(3, 4).rows(0, 3)
+    val newCenterVector = wristCenter.cols(3, 4).rows(0, 3)
+
+    assert((wristCenterVector - newCenterVector).length() < 0.001)
+  }
+
+  @Test
+  fun `test compute puma theta 23` () {
+    val wristCenter = pumaParams.subList(0, 4).forwardKinematics(arrayOf(0.0, 0.0, 0.0, 0.0).toDoubleArray())
+    val thetas23 = pumaParams.computeTheta23(wristCenter, 0.0)
+
+    val newCenter = pumaParams.subList(0, 4).forwardKinematics(arrayOf(0.0, thetas23[1][0], thetas23[1][1], 0.0).toDoubleArray())
+
+    val wristCenterVector = wristCenter.cols(3, 4).rows(0, 3)
+    val newCenterVector = wristCenter.cols(3, 4).rows(0, 3)
+
+    assert((wristCenterVector - newCenterVector).length() < 0.001)
   }
 }
