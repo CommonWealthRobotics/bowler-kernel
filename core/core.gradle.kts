f5cb42c849500afd8146183b6a9639e6d6579dd1
Ryan Benasutti <firey45@gmail.com>
2019-03-03 14:25:25 -0500

    Make the kernel a test dependency

--- core/core.gradle.kts
+++ core/core.gradle.kts
@@ -1,4 +1,4 @@
-import Core_gradle.Verions.arrow_version
+import Core_gradle.Versions.arrow_version
 
 plugins {
     `java-library`
@@ -6,17 +6,12 @@ plugins {
 
 description = "The core module."
 
-object Verions {
+object Versions {
     const val arrow_version = "0.7.3"
 }
 
 dependencies {
-    api(group = "com.neuronrobotics", name = "BowlerScriptingKernel", version = "0.32.4") {
-        exclude(group = "org.slf4j", module = "slf4j-simple")
-        exclude(group = "com.google.guava")
-        exclude(group = "org.mockito")
-    }
-
+    api(group = "com.neuronrobotics", name = "java-bowler", version = "3.26.2")
     api(group = "org.apache.commons", name = "commons-math3", version = "3.6.1")
     api(group = "org.ejml", name = "ejml-all", version = "0.36")
 
@@ -44,17 +39,23 @@ dependencies {
     implementation(group = "com.google.guava", name = "guava", version = "25.0-jre")
     implementation(group = "com.google.inject", name = "guice", version = "4.1.0")
     implementation(
-        group = "com.google.inject.extensions",
-        name = "guice-assistedinject",
-        version = "4.1.0"
+            group = "com.google.inject.extensions",
+            name = "guice-assistedinject",
+            version = "4.1.0"
     )
     implementation(group = "org.jlleitschuh.guice", name = "kotlin-guiced-core", version = "0.0.5")
     implementation(group = "com.google.code.findbugs", name = "annotations", version = "3.0.1")
     implementation(group = "com.natpryce", name = "hamkrest", version = "1.4.2.2")
 
     testImplementation(
-        group = "com.nhaarman.mockitokotlin2",
-        name = "mockito-kotlin",
-        version = "2.0.0"
+            group = "com.nhaarman.mockitokotlin2",
+            name = "mockito-kotlin",
+            version = "2.0.0"
     )
+
+    testImplementation(group = "com.neuronrobotics", name = "BowlerScriptingKernel", version = "0.32.4") {
+        exclude(group = "org.slf4j", module = "slf4j-simple")
+        exclude(group = "com.google.guava")
+        exclude(group = "org.mockito")
+    }
 }
