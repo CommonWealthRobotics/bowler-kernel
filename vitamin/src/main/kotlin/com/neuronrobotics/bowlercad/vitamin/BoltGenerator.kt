95c4694474c49835f6653ada31a720a58bb05f3f
Ryan Benasutti <firey45@gmail.com>
2019-06-04 18:27:31 -0400

    Add BoltGenerator

---
+++ vitamin/src/main/kotlin/com/neuronrobotics/bowlercad/vitamin/BoltGenerator.kt
@@ -0,0 +1,47 @@
+/*
+ * This file is part of bowler-cad.
+ *
+ * bowler-cad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bowler-cad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with bowler-cad.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.neuronrobotics.bowlercad.vitamin
+
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.Bolt
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+import org.octogonapus.ktunits.quantities.millimeter
+
+class BoltGenerator(
+    maxCacheSize: Long = 100
+) : VitaminCadGenerator<Bolt> {
+
+    private val cache = CacheBuilder.newBuilder()
+        .maximumSize(maxCacheSize)
+        .build(CacheLoader.from<Bolt, CSG> {
+            it!!
+
+            val head = Cylinder(it.headDiameter.millimeter / 2, it.headHeight.millimeter)
+                .toCSG()
+                .toZMin()
+
+            val body = Cylinder(it.throughHoleDiameter.millimeter / 2, it.bodyHeight.millimeter)
+                .toCSG()
+                .toZMax()
+
+            body.union(head).toZMin()
+        })
+
+    override fun generateCAD(vitamin: Bolt): CSG = cache[vitamin]
+}
