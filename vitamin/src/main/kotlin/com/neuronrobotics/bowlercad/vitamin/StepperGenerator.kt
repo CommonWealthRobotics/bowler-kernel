ad124a33d360f09a7944855eebd937fcbfdca012
Ryan Benasutti <firey45@gmail.com>
2019-06-04 18:20:17 -0400

    Properly implement stepper generator

---
+++ vitamin/src/main/kotlin/com/neuronrobotics/bowlercad/vitamin/StepperGenerator.kt
@@ -0,0 +1,63 @@
+/*
+ * This file is part of bowler-cad.
+ *
+ * bowler-cad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bowler-cad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with bowler-cad.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.neuronrobotics.bowlercad.vitamin
+
+import com.google.common.cache.CacheBuilder
+import com.google.common.cache.CacheLoader
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.Bolt
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.Shaft
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.StepperMotor
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import org.octogonapus.ktunits.quantities.millimeter
+
+class StepperGenerator(
+    boltGenerator: VitaminCadGenerator<Bolt>,
+    shaftGenerator: VitaminCadGenerator<Shaft>,
+    maxCacheSize: Long = 100
+) : VitaminCadGenerator<StepperMotor> {
+
+    private val cache = CacheBuilder.newBuilder()
+        .maximumSize(maxCacheSize)
+        .build(CacheLoader.from<StepperMotor, CSG> {
+            it!!
+
+            val body = Cube(it.width.millimeter, it.width.millimeter, it.height.millimeter)
+                .toCSG()
+                .toZMax()
+
+            val bolt = boltGenerator.generateCAD(it.bolt).toZMax()
+            val bolts = CSG.unionAll(
+                mutableListOf(
+                    bolt.movex(it.boltHoleSpacing.millimeter / 2)
+                        .movey(it.boltHoleSpacing.millimeter / 2),
+                    bolt.movex(-it.boltHoleSpacing.millimeter / 2)
+                        .movey(it.boltHoleSpacing.millimeter / 2),
+                    bolt.movex(it.boltHoleSpacing.millimeter / 2)
+                        .movey(-it.boltHoleSpacing.millimeter / 2),
+                    bolt.movex(-it.boltHoleSpacing.millimeter / 2)
+                        .movey(-it.boltHoleSpacing.millimeter / 2)
+                )
+            )
+
+            val shaft = shaftGenerator.generateCAD(it.shaft).toZMin()
+
+            body.difference(bolts).union(shaft)
+        })
+
+    override fun generateCAD(vitamin: StepperMotor): CSG = cache[vitamin]
+}
---
+++ vitamin/src/test/kotlin/com/neuronrobotics/bowlercad/vitamin/StepperGeneratorTest.kt
@@ -0,0 +1,124 @@
+/*
+ * This file is part of bowler-cad.
+ *
+ * bowler-cad is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bowler-cad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with bowler-cad.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.neuronrobotics.bowlercad.vitamin
+
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.Bolt
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.CenterOfMass
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.DefaultBolt
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.DefaultShaft
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.DefaultStepperMotor
+import com.neuronrobotics.bowlerkernel.vitamins.vitamin.Shaft
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cylinder
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
+import org.octogonapus.ktguava.collections.emptyImmutableMap
+import org.octogonapus.ktunits.quantities.ampere
+import org.octogonapus.ktunits.quantities.degree
+import org.octogonapus.ktunits.quantities.gram
+import org.octogonapus.ktunits.quantities.inch
+import org.octogonapus.ktunits.quantities.millimeter
+import org.octogonapus.ktunits.quantities.minus
+import org.octogonapus.ktunits.quantities.nM
+import org.octogonapus.ktunits.quantities.plus
+import org.octogonapus.ktunits.quantities.volt
+
+internal class StepperGeneratorTest {
+
+    private val tolerance = 1e-8
+    private val generator = StepperGenerator(
+        boltGenerator = object : VitaminCadGenerator<Bolt> {
+            override fun generateCAD(vitamin: Bolt): CSG =
+                Cylinder(1.5, (vitamin.headHeight + vitamin.bodyHeight).millimeter).toCSG()
+        },
+        shaftGenerator = object : VitaminCadGenerator<Shaft> {
+            override fun generateCAD(vitamin: Shaft): CSG =
+                Cylinder(3.0, vitamin.height.millimeter).toCSG()
+        }
+    )
+
+    @Test
+    fun `test servo`() {
+        val stepper = DefaultStepperMotor(
+            width = 40.millimeter,
+            height = 37.millimeter,
+            boltHoleSpacing = 31.millimeter,
+            bolt = DefaultBolt(
+                headDiameter = 5.35.millimeter,
+                headHeight = 2.93.millimeter,
+                throughHoleDiameter = 2.93.millimeter,
+                bodyHeight = 15.millimeter,
+                mass = 10.gram,
+                centerOfMass = CenterOfMass(
+                    0.inch,
+                    0.inch,
+                    0.inch
+                ),
+                specs = emptyImmutableMap()
+            ),
+            shaft = DefaultShaft.ServoHorn.Arm(
+                baseDiameter = 8.millimeter,
+                tipDiameter = 3.millimeter,
+                baseCenterToTipCenterLength = 22.5.millimeter,
+                thickness = 2.millimeter,
+                baseColumnThickness = 5.4.millimeter,
+                points = 1,
+                mass = 0.1.gram,
+                centerOfMass = CenterOfMass(
+                    0.inch,
+                    0.inch,
+                    0.inch
+                ),
+                specs = emptyImmutableMap()
+            ),
+            voltage = 12.volt,
+            holdingTorque = 15.8.nM,
+            current = 0.31.ampere,
+            stepAngle = 1.8.degree,
+            mass = 58.gram,
+            centerOfMass = CenterOfMass(
+                0.inch,
+                0.inch,
+                0.inch
+            ),
+            specs = emptyImmutableMap()
+        )
+
+        val cad = generator.generateCAD(stepper)
+
+        assertAll(
+            { assertEquals(0.0, cad.bounds.center.x) },
+            { assertEquals(0.0, cad.bounds.center.y) },
+            {
+                assertEquals(
+                    (stepper.shaft.height - stepper.height).millimeter / 2,
+                    cad.bounds.center.z,
+                    tolerance
+                )
+            },
+            { assertEquals(stepper.width.millimeter, cad.bounds.bounds.x) },
+            { assertEquals(stepper.width.millimeter, cad.bounds.bounds.y) },
+            {
+                assertEquals(
+                    (stepper.height + stepper.shaft.height).millimeter,
+                    cad.bounds.bounds.z
+                )
+            }
+        )
+    }
+}
