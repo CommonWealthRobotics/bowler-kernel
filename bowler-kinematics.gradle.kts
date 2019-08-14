1ef8244ff909ba3cc0206e4c313f6ac1b065ecbe
Ryan Benasutti <firey45@gmail.com>
2019-08-14 17:38:15 -0400

    Version bump

--- bowler-kinematics.gradle.kts
+++ bowler-kinematics.gradle.kts
@@ -24,7 +24,7 @@ plugins {
 
 object Versions {
     const val ktlintVersion = "0.29.0"
-    const val bowlerKinematicsVersion = "0.1.1"
+    const val bowlerKinematicsVersion = "0.2.0"
 }
 
 allprojects {
