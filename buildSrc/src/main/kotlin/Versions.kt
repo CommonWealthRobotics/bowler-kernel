fe9a62c20487d9d0530628c6d71cef48e4661edd
Octogonapus <firey45@gmail.com>
2020-10-12 01:49:55 -0400

    Fix syntax

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -20,7 +20,7 @@
  * blocks.
  */
 object Versions {
-    const val bowlerKernel = "0.3.10"
+    const val bowlerKernel = "0.3.11"
 
     const val spotlessPlugin = "5.6.1"
     const val testLoggerPlugin = "2.1.0"
