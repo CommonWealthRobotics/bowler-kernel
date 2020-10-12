0e05b0696fc97c1e7bbb015ea1e7d37fc47e5e1f
Octogonapus <firey45@gmail.com>
2020-10-12 13:19:28 -0400

    Fix pre-release detection. Fix bintray secrets.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -20,7 +20,7 @@
  * blocks.
  */
 object Versions {
-    const val bowlerKernel = "0.3.30"
+    const val bowlerKernel = "0.3.31"
 
     const val spotlessPlugin = "5.6.1"
     const val testLoggerPlugin = "2.1.0"
