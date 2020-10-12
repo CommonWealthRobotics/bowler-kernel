3f824411117390b062bce08872cf20147123242d
Octogonapus <firey45@gmail.com>
2020-10-12 18:59:00 -0400

    Fix license header on Metadata file

--- buildSrc/src/main/kotlin/Metadata.kt
+++ buildSrc/src/main/kotlin/Metadata.kt
@@ -1,3 +1,24 @@
+/*
+ * This file is part of bowler-kernel.
+ *
+ * bowler-kernel is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * bowler-kernel is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with bowler-kernel.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
+ * This file cannot be in a package. That would prevent Gradle from loading it inside `plugins {}`
+ * blocks.
+ */
 object Metadata {
     const val projectName = "bowler-kernel"
     const val projectDescription = "The heart of the Bowler stack."
